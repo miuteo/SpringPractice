230779a682a411d8fc29e25776ea2f0dd6b0489a
Teo <miu.teo@gmail.com>
2016-11-16 21:36:36 +0200

    Spring start

---
+++ src/chapter2/CDPlayerConfig.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Ionela on 11/16/2016.
+ */
+@Configuration
+@ComponentScan
+public class CDPlayerConfig {
+}
