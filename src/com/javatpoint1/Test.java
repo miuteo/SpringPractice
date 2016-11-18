ac8af2dbc07a4f3ce57c65652745a4fe3014cbe2
Teo <miu.teo@gmail.com>
2016-11-18 21:41:43 +0200

    Spring start using Context

--- src/com/javatpoint1/Test.java
+++ src/com/javatpoint1/Test.java
@@ -2,6 +2,8 @@ package com.javatpoint1;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 
@@ -10,10 +12,13 @@ import org.springframework.core.io.Resource;
  */
 public class Test {
     public static void main(String[]args){
-        Resource resource = new ClassPathResource("com\\javatpoint1\\applicationContext.xml");
-        BeanFactory factory = new XmlBeanFactory(resource);
+//        Resource resource = new ClassPathResource("com\\javatpoint1\\applicationContext.xml");
+//        BeanFactory factory = new XmlBeanFactory(resource);
 
-        Student student = (Student) factory.getBean("studentbean");
+        ApplicationContext context = new ClassPathXmlApplicationContext("com\\\\javatpoint1\\\\applicationContext.xml");
+
+//        Student student = (Student) factory.getBean("studentbean");
+        Student student = (Student) context.getBean("studentbean");
         student.displayInfo();
     }
 }
