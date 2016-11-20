520a4d52357082d8ab463eb17822c535f1bdd732
Teo <miu.teo@gmail.com>
2016-11-20 16:15:08 +0200

    Constructor Injection with Collection

---
+++ src/com/javatpoint/DI/CIWithCollection/Question.java
@@ -0,0 +1,20 @@
+package com.javatpoint.DI.CIWithCollection;
+
+import java.util.List;
+
+/**
+ * Created by Ionela on 11/20/2016.
+ */
+public class Question {
+    private int id;
+    private String name;
+    private List<String> answers;
+    public Question(int id,String name,List<String>answers){
+        this.id = id;
+        this.name = name;
+        this.answers = answers;
+    }
+    public String toString(){
+        return(id +" " + name +"\nanswers are:"+answers);
+    }
+}
