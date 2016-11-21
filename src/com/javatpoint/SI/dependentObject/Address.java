615767f7eb0d651745742a88a6d99008e8dfe191
Teo <miu.teo@gmail.com>
2016-11-21 16:20:10 +0200

    Setter Injection - Dependent Object

---
+++ src/com/javatpoint/SI/dependentObject/Address.java
@@ -0,0 +1,46 @@
+package com.javatpoint.SI.dependentObject;
+
+/**
+ * Created by Ionela on 11/21/2016.
+ */
+public class Address {
+    private String addressLine1;
+    private String city;
+    private String state;
+    private String country;
+
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    public String toString() {
+        return addressLine1 + " " + city + " " + state + " " + country;
+    }
+}
