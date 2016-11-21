8c92c0cb8da89bf7e446a5704dde876f91735d3d
Teo <miu.teo@gmail.com>
2016-11-21 17:03:11 +0200

    Setter Injection - Collection2

---
+++ src/com/javatpoint/SI/Collection2/Question.java
@@ -0,0 +1,45 @@
+package com.javatpoint.SI.Collection2;
+
+import java.util.List;
+
+/**
+ * Created by Ionela on 11/21/2016.
+ */
+public class Question {
+    private int id;
+    private String name;
+    private List<Answer> answers;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Answer> getAnswers() {
+        return answers;
+    }
+
+    public void setAnswers(List<Answer> answers) {
+        this.answers = answers;
+    }
+
+    @Override
+    public String toString() {
+        return "Question{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", answers=" + answers +
+                '}';
+    }
+}
