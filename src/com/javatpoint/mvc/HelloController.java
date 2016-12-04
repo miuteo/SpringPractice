fe692223229bae2044587c0e465309df7dcebd04
Teo Miu - Asus x556 <miu.teo@gmail.com>
2016-12-04 13:27:03 +0200

    Spring MVC

---
+++ src/com/javatpoint/mvc/HelloController.java
@@ -0,0 +1,13 @@
+package com.javatpoint.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class HelloController {
+    @RequestMapping("/hello")
+    public ModelAndView helloWorld() {
+        String message = "HELLO SPRING MVC HOW R U";
+        return new ModelAndView("hellopage", "message", message);
+    }
+}
---
+++ src/com/javatpoint/mvc/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.javatpoint.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by Asus x556 on 04-Dec-16.
+ */
+@Controller
+public class WelcomeController {
+    @RequestMapping("/welcome")
+    public ModelAndView welcome(){
+        return new ModelAndView("welcomepage");
+    }
+}
