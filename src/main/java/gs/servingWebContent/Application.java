42854151286631cbac34c985d1a7179b663064e3
teodor.miu <miu.teo@gmail.com>
2017-06-06 14:34:39 +0300

    reset2

---
+++ src/main/java/gs/servingWebContent/Application.java
@@ -0,0 +1,16 @@
+package gs.servingWebContent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by teodor.miu on 06-Jun-17.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[]args){
+        SpringApplication.run(Application.class,args);
+    }
+
+}
