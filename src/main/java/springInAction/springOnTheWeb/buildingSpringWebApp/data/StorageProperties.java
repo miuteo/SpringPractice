38d84fac2d6a18f6c7494d7012588a0ca7b0a0fd
teodor.miu <miu.teo@gmail.com>
2017-06-20 17:29:18 +0300

    Storage picture

---
+++ src/main/java/springInAction/springOnTheWeb/buildingSpringWebApp/data/StorageProperties.java
@@ -0,0 +1,20 @@
+package springInAction.springOnTheWeb.buildingSpringWebApp.data;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+
+/**
+ * Created by teodor.miu on 20-Jun-17.
+ */
+@ConfigurationProperties("storage")
+public class StorageProperties {
+    private String location = "upload-dir";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
