67bad303fd5b42b28ad317b5ae4a2c954aca0f62
Teo Miu - Asus x556 <miu.teo@gmail.com>
2017-06-26 23:16:20 +0300

    logout

--- src/main/java/springInAction/springOnTheWeb/buildingSpringWebApp/web/HomeController.java
+++ src/main/java/springInAction/springOnTheWeb/buildingSpringWebApp/web/HomeController.java
@@ -1,8 +1,14 @@
 package springInAction.springOnTheWeb.buildingSpringWebApp.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by Asus x556 on 18-Jun-17.
@@ -13,5 +19,14 @@ public class HomeController {
     @RequestMapping(method = RequestMethod.GET)
     public String home(){
         return "home";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";//You can redirect wherever you want, but generally it's a good practice to show login screen again.
     }
 }
