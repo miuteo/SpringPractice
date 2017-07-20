6fdb895ca64cf068d762677e4c66b5ea9ee6d1a8
teodor.miu <miu.teo@gmail.com>
2017-07-20 17:57:25 +0300

    mongoDB

---
+++ src/main/java/springInAction/noSql/mongDB/config/MongoConfig.java
@@ -0,0 +1,22 @@
+package springInAction.noSql.mongDB.config;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "springInAction.noSql.mongDB")
+public class MongoConfig extends AbstractMongoConfiguration{
+
+    @Override
+    protected String getDatabaseName() {
+        return "OrdersDB";
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient();
+    }
+}
